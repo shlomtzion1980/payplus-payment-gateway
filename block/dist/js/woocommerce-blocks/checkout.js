fb94031ada112bcc15c7722db84b2c2485c38821
shlomtzion <shlomtzion@gmai.com>
2024-05-11 21:36:42 +0300

    design!

--- block/dist/js/woocommerce-blocks/checkout.js
+++ block/dist/js/woocommerce-blocks/checkout.js
@@ -1,8 +1,3 @@
-// const settings = window.wc.wcSettings.getPaymentMethodData(
-//   "payplus-payment-gateway",
-//   {}
-// );
-
 const gateways = [
   "payplus-payment-gateway",
   "payplus-payment-gateway-bit",
@@ -14,44 +9,6 @@ const gateways = [
   "payplus-payment-gateway-finitione",
   "payplus-payment-gateway-paypal",
 ];
-
-// console.log(settings);
-
-// const label =
-//   window.wp.htmlEntities.decodeEntities(settings.title) ||
-//   window.wp.i18n.__("Pay with Debit or Credit Card", "payplus-payment-gateway");
-
-// console.log(window.wp.htmlEntities.decodeEntities(settings.description || ""));
-// // create an element with the title and an img element with the icon in the payment method
-// const PaymentMethodLabel = ({ text, icon }) => {
-//   return window.wp.element.createElement(
-//     "div",
-//     { className: "payplus-method" },
-//     window.wp.element.createElement("img", {
-//       style: {
-//         width: "64px",
-//         height: "32px",
-//         maxHeight: "100%",
-//         margin: "0px 10px",
-//       },
-//       src: settings.icon,
-//     }),
-//     label,
-//     window.wp.element.createElement("div", { className: "pp_iframe" })
-//   );
-// };
-// const Content = () => {
-//   return window.wp.element.createElement(
-//     "div",
-//     null,
-//     window.wp.element.createElement(
-//       "span",
-//       null,
-//       window.wp.htmlEntities.decodeEntities(settings.description || "")
-//     ),
-//     window.wp.element.createElement("div", { className: "pp_iframe" })
-//   );
-// };
 
 for (let c = 0; c < gateways.length; c++) {
   const gateway = gateways[c];
@@ -67,7 +24,13 @@ for (let c = 0; c < gateways.length; c++) {
   const PaymentMethodLabel = ({ text, icon }) => {
     return window.wp.element.createElement(
       "div",
-      { className: "payplus-method" },
+      {
+        className: "payplus-method-label",
+        style: {
+          display: "flex",
+          alignItems: "center",
+        },
+      },
       window.wp.element.createElement("img", {
         style: {
           width: "64px",
@@ -77,14 +40,13 @@ for (let c = 0; c < gateways.length; c++) {
         },
         src: settings.icon,
       }),
-      label,
-      window.wp.element.createElement("div", { className: "pp_iframe" })
+      label
     );
   };
   const Content = () => {
     return window.wp.element.createElement(
       "div",
-      null,
+      { className: "payplus-method" },
       window.wp.element.createElement(
         "span",
         null,
@@ -108,16 +70,3 @@ for (let c = 0; c < gateways.length; c++) {
   };
   window.wc.wcBlocksRegistry.registerPaymentMethod(Block_Gateway);
 }
-// const Block_Gateway = {
-//   name: "payplus-payment-gateway",
-//   label: Object(window.wp.element.createElement)(PaymentMethodLabel, null),
-//   content: Object(window.wp.element.createElement)(Content, null),
-//   edit: Object(window.wp.element.createElement)(Content, null),
-//   canMakePayment: () => true,
-//   ariaLabel: label,
-//   supports: {
-//     features: settings.supports,
-//     showSaveOption: settings.showSaveOption,
-//   },
-// };
-// window.wc.wcBlocksRegistry.registerPaymentMethod(Block_Gateway);
