82bf4130178a190fcad46712bc1508882a3d3cb8
shlomtzion <shlomtzion@gmai.com>
2024-05-13 16:24:20 +0300

    change/removedThankyou save CC

--- payplus-payment-gateway.php
+++ payplus-payment-gateway.php
@@ -51,7 +51,6 @@ class WC_PayPlus
 
         add_action('woocommerce_before_checkout_form', [$this, 'msg_checkout_code']);
         add_action('woocommerce_order_status_changed', [$this, 'payplus_order_status_changed_action'], 50, 4);
-        add_action('woocommerce_thankyou', [$this, 'thankyou_check_save_tokens'], 10, 1);
         add_action('wp_ajax_payplus-check-tokens', [$this, 'ajax_payplus_check_tokens']);
         add_action('wp_ajax_payplus-delete-token', [$this, 'ajax_payplus_delete_token']);
 
@@ -142,76 +141,6 @@ class WC_PayPlus
         wp_die();
     }
 
-    /**
-     * Check if the the cc token is new and if so display the save token message
-     * @param int $order_id
-     * @param string $status
-     * @param string $old_status
-     * @param WC_Order $order
-     * @return void
-     */
-    public function thankyou_check_save_tokens($order_id)
-    {
-
-        $order = wc_get_order($order_id);
-        if ($order->get_payment_method() === 'payplus-payment-gateway' && $this->payplus_payment_gateway_settings->save_pp_token_receipt_page === 'yes' && $this->payplus_payment_gateway_settings->enabled === 'yes') {
-            // Get the order object
-            $user_id = $order->get_user_id();
-            $order_meta = WC_PayPlus_Order_Data::get_meta($order, ['payplus_response', 'payplus_token_saved']);
-            $data = isset($order_meta['payplus_response']) ? json_decode($order_meta['payplus_response'], true) : null;
-            $tokenUid = isset($data['token_uid']) ? $data['token_uid'] : null;
-            $tokenSaved = isset($order_meta['payplus_token_saved']) ? $order_meta['payplus_token_saved'] : false;
-            $customerTokens = WC_Payment_Tokens::get_customer_tokens($user_id);
-            $theTokens = [];
-
-            foreach ($customerTokens as $customerToken) {
-                $theTokens[] = $customerToken->get_token();
-            };
-
-            if (!in_array($tokenUid, $theTokens) && $tokenUid != null && !$tokenSaved) {
-                // call thankyou.js and register the script
-                wp_register_script('thankyou-js', PAYPLUS_PLUGIN_URL . '/assets/js/thankyou.js', ['jquery'], time(), true);
-                wp_localize_script(
-                    'thankyou-js',
-                    'payplus_script_thankyou',
-                    array(
-                        'ajax_url' => admin_url('admin-ajax.php'),
-                        'userId' => $user_id,
-                        'orderId' => $order_id,
-                        'token' => $tokenUid,
-                    )
-                );
-                wp_enqueue_script('thankyou-js');
-?>
-                <div id="newToken" class="payplus_thankyou-new-token">
-                    <div class="payplus_save_token_messsage">
-                        <?php echo __('Would you like to save this credit card securely to you account, for future purchases?', 'payplus-payment-gateway'); ?>
-                    </div>
-                    <form action="" method="post">
-                        <input type="hidden" name="token" value="<?php echo $tokenUid; ?>">
-                        <input type="hidden" id="user_id" value="<?php echo $user_id; ?>">
-                        <input type="hidden" id="order_id" value="<?php echo $order_id; ?>">
-                        <input type="submit" name="saveToken" value="<?php echo __('Yes', 'payplus-payment-gateway'); ?>">
-                        <input type="submit" name="deleteToken" value="<?php echo __('No', 'payplus-payment-gateway'); ?>">
-                        <div class="payplus_loader">
-                            <div class="loader">
-                                <div class="loader-background">
-                                    <div class="text"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                    <div class="payplus_icon"></div>
-                </div>
-            <?php
-            }
-            if (isset($_POST['saveToken'])) {
-                $this->payplus_gateway = $this->get_main_payplus_gateway();
-                $this->payplus_gateway->save_token($data, $user_id);
-                WC_PayPlus_Order_Data::update_meta($order, ['payplus_token_saved' => true]);
-            }
-        }
-    }
 
     /**
      * @return void
@@ -317,7 +246,7 @@ class WC_PayPlus
         $error_page_payplus = get_option('error_page_payplus');
         $postIdcurrenttUrl = url_to_postid(home_url($wp->request));
         if (intval($postIdcurrenttUrl) === intval($error_page_payplus)) {
-            ?>
+?>
             <meta name=" robots" content="noindex,nofollow">
         <?php
         }
