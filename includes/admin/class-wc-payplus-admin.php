74d56814d8e36569a86370b1a36ad01417d4459a
ShlomtzPP <shlomtzionPP>
2024-08-26 11:15:54 +0300

    feature/CurrentLanguage - and minified

--- includes/admin/class-wc-payplus-admin.php
+++ includes/admin/class-wc-payplus-admin.php
@@ -2093,7 +2093,7 @@ class WC_PayPlus_Admin_Payments extends WC_PayPlus_Gateway
         $transactionType = $this->get_option('transaction_type');
 
         wp_enqueue_style('payplus', PAYPLUS_PLUGIN_URL . 'assets/css/admin.min.css', [], PAYPLUS_VERSION);
-        wp_register_script('payplus-admin-payment', PAYPLUS_PLUGIN_URL . '/assets/js/admin-payments.js', ['jquery'], time(), true);
+        wp_register_script('payplus-admin-payment', PAYPLUS_PLUGIN_URL . '/assets/js/admin-payments.min.js', ['jquery'], time(), true);
         wp_localize_script(
             'payplus-admin-payment',
             'payplus_script_admin',
