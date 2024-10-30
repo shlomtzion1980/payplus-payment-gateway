e5b949418a954ac2c086a170f292c60c5e009be8
shlomtzion <shlomtzion@gmai.com>
2024-10-30 17:34:28 +0200

    tweak/fixed powered by

--- templates/hostedFields.php
+++ templates/hostedFields.php
@@ -16,381 +16,382 @@ $opposite = $locale !== "he_IL" ?  "left" : "right";
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <style>
-        @font-face {
-            font-family: 'AlmoniMLv5AAA';
-            src: url('../wp-content/plugins/payplus-payment-gateway/assets/css/fonts/almoni-medium-aaa.woff2') format('woff2'),
-                url('../wp-content/plugins/payplus-payment-gateway/assets/css/fonts/almoni-medium-aaa.woff') format('woff'),
-                url('../wp-content/plugins/payplus-payment-gateway/assets/css/fonts/almoni-medium-aaa.eot') format('opentype');
-            font-weight: normal;
-            /* Or bold if necessary */
-            font-style: normal;
-            /* Or italic if necessary */
+    @font-face {
+        font-family: 'AlmoniMLv5AAA';
+        src: url('../wp-content/plugins/payplus-payment-gateway/assets/css/fonts/almoni-medium-aaa.woff2') format('woff2'),
+            url('../wp-content/plugins/payplus-payment-gateway/assets/css/fonts/almoni-medium-aaa.woff') format('woff'),
+            url('../wp-content/plugins/payplus-payment-gateway/assets/css/fonts/almoni-medium-aaa.eot') format('opentype');
+        font-weight: normal;
+        /* Or bold if necessary */
+        font-style: normal;
+        /* Or italic if necessary */
+    }
+
+    label {
+        margin-bottom: 6px !important;
+        border: none !important;
+    }
+
+    .fld-frame {
+        border: 1px solid #ced4da;
+        height: 37px;
+        padding: 5px;
+        margin: 5px 0;
+        background-color: #fff;
+        width: 100%;
+    }
+
+    .hf-col-4 {
+        flex: 0 0 auto;
+        width: fit-content !important;
+        display: block;
+        background: #F7F7F7;
+        padding: 15px;
+        border-radius: 10px;
+        border: 1px solid #E3E6E9 !important;
+        position: relative;
+        flex-direction: row;
+        /* align-content: center; */
+        flex-wrap: wrap;
+        justify-content: flex-end;
+        margin: auto;
+        display: flex;
+        /* flex-direction: column; */
+        background: #F7F7F7;
+        /* height: 55vh; */
+        flex-wrap: wrap;
+        justify-content: space-between;
+        font-family: 'AlmoniMLv5AAA';
+        min-height: 533px;
+
+        #payments {
+            background-color: white !important;
         }
 
-        label {
-            margin-bottom: 6px !important;
-            border: none !important;
-        }
-
-        .fld-frame {
-            border: 1px solid #ced4da;
-            height: 37px;
-            padding: 5px;
-            margin: 5px 0;
-            background-color: #fff;
-            width: 100%;
-        }
-
-        .hf-col-4 {
-            flex: 0 0 auto;
-            width: fit-content !important;
-            display: block;
-            background: #F7F7F7;
-            padding: 15px;
-            border-radius: 10px;
-            border: 1px solid #E3E6E9 !important;
-            position: relative;
-            flex-direction: row;
-            /* align-content: center; */
-            flex-wrap: wrap;
-            justify-content: flex-end;
-            margin: auto;
-            display: flex;
-            /* flex-direction: column; */
-            background: #F7F7F7;
-            /* height: 55vh; */
-            flex-wrap: wrap;
-            justify-content: space-between;
-            font-family: 'AlmoniMLv5AAA';
-            min-height: 533px;
-
-            #payments {
+        .hf-row {
+            select {
                 background-color: white !important;
             }
-
-            .hf-row {
-                select {
-                    background-color: white !important;
-                }
-            }
-
-            select {
-
-                padding-right: 15px !important;
-                padding-left: 15px !important;
-                padding-top: unset !important;
-                padding-bottom: unset !important;
-                font-size: 1rem !important;
-
-                @media screen and (min-width: 567px) {
-                    appearance: none;
-                    /* Remove default arrow (Webkit) */
-                    -moz-appearance: none;
-                    /* Remove default arrow (Firefox) */
-
-                    background-image: url('../wp-content/plugins/payplus-payment-gateway/assets/images/dropdown-arrow.png');
-                    background-repeat: no-repeat;
-                    background-position: <?php echo esc_attr($direction); ?> 15px center;
-                    /* Add custom arrow */
-                    background-size: 10px;
-                }
-            }
         }
 
-        #hostedTop {
-            display: flex;
-            height: fit-content;
-            justify-content: space-between;
-            border-bottom: 1px solid #E3E6E9 !important;
+        select {
+
+            padding-right: 15px !important;
+            padding-left: 15px !important;
+            padding-top: unset !important;
+            padding-bottom: unset !important;
+            font-size: 1rem !important;
+
+            @media screen and (min-width: 567px) {
+                appearance: none;
+                /* Remove default arrow (Webkit) */
+                -moz-appearance: none;
+                /* Remove default arrow (Firefox) */
+
+                background-image: url('../wp-content/plugins/payplus-payment-gateway/assets/images/dropdown-arrow.png');
+                background-repeat: no-repeat;
+                background-position: <?php echo esc_attr($direction);
+                ?>15px center;
+                /* Add custom arrow */
+                background-size: 10px;
+            }
+        }
+    }
+
+    #hostedTop {
+        display: flex;
+        height: fit-content;
+        justify-content: space-between;
+        border-bottom: 1px solid #E3E6E9 !important;
+        width: 100%;
+        padding: 20px;
+
+        .topText {
+            color: #000000;
+            font-size: 16px;
+        }
+
+        .creditCards {
+            img {
+                max-height: 60px;
+            }
+        }
+    }
+
+    #submit-payment {
+        background-color: var(--wp--preset--color--contrast);
+        border-radius: 0.33rem;
+        border-color: var(--wp--preset--color--contrast);
+        border-width: 0;
+        color: var(--wp--preset--color--base);
+        font-family: inherit;
+        font-size: var(--wp--preset--font-size--small);
+        font-style: normal;
+        font-weight: 500;
+        line-height: inherit;
+        padding-top: 0.6rem;
+        padding-right: 1rem;
+        padding-bottom: 0.6rem;
+        padding-left: 1rem;
+        text-decoration: none;
+        margin-top: 15px !important;
+        margin-left: auto !important;
+        margin-right: auto !important;
+        border-radius: 8px;
+        width: 90%;
+    }
+
+    #ppLogo {
+        font-family: "Almoni", sans-serif;
+        color: #c5cbcf;
+        text-align: center;
+        font-size: 12px;
+        width: 100%;
+        bottom: 5px;
+        direction: ltr;
+        padding: 1em;
+
+        .hf-image {
+            height: 17px;
+            top: 2px;
+            display: initial;
+        }
+    }
+
+    .expiry-wrapper {
+        justify-content: space-between;
+    }
+
+    .container.hostedFields {
+        display: none;
+    }
+
+    #payment-form {
+        display: none;
+        align-items: center;
+        flex-direction: column;
+        flex-wrap: wrap;
+        align-content: flex-start;
+    }
+
+    .__payplus_hosted_fields_err_fld {
+        color: red;
+        border: 1px solid red;
+    }
+
+    input[readonly] {
+        background-color: #eee;
+        outline: 0;
+    }
+
+    .blocks-payplus_loader_hosted {
+        display: none;
+        position: relative;
+        z-index: 999999;
+    }
+
+    .__payplus_hosted_fields_item_fld-wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        align-content: center;
+    }
+
+    .expiries {
+        width: 90%;
+        display: flex;
+        margin: auto;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+    input:-internal-autofill-selected {
+        background-color: transparent !important;
+    }
+
+    .col-9 {
+        @media screen and (min-width: 467px) {
+            margin-left: 3px;
+            max-width: 74% !important;
+        }
+    }
+
+    .expireCvvClass {
+        background-color: white;
+        width: 50%;
+        height: 45px;
+        display: flex;
+        padding: 0 10px 0 10px;
+        justify-content: center;
+        border: 1px solid #E3E6E9 !important;
+        border-top: none !important;
+
+
+        &.right {
+            border-bottom-right-radius: 8px;
+            border-left: none !important;
+        }
+
+        &.left {
+            border-bottom-left-radius: 8px;
+        }
+    }
+
+    .smallCol {
+        flex: 0 0 auto;
+        width: 30%;
+    }
+
+    .justBorder {
+        border: 1px solid #E3E6E9 !important;
+    }
+
+    .pp_iframe_h iframe {
+        border: 0;
+        max-height: 45px;
+        /* min-width: 55px;
+            text-align: center; */
+    }
+
+    .fld-frame {
+        border: none;
+        box-sizing: unset;
+        outline: unset;
+    }
+
+    input {
+        outline: unset;
+    }
+
+    .pp_iframe_h {
+        .form-control {
+            width: 100% !important;
+            padding: 0.5rem 0.75rem !important;
+            font-size: 1rem !important;
+            border: 1px solid #E3E6E9 !important;
+            border-top-left-radius: 8px;
+            border-top-right-radius: 8px;
+            height: 45px;
+            margin: auto !important;
+            text-align: inherit;
+        }
+
+        .forms-control {
+            width: 100% !important;
+            padding: 0.5rem 0.75rem !important;
+            font-size: 1rem !important;
+            border: 1px solid #E3E6E9 !important;
+            border-radius: 8px !important;
+            height: 45px;
+            margin: auto !important;
+            text-align: inherit;
+        }
+    }
+
+    input[type="text"i] {
+        font-size: 20px;
+    }
+
+    .h-fld-wrapper {
+        width: 100%;
+    }
+
+    .fld-wrapper {
+        width: 90%;
+        margin: 0.2em auto 0.2em auto;
+    }
+
+    .hf-row,
+    .hf-main {
+        width: 100%;
+        display: flex;
+
+        @media screen and (max-width: 768px) {
+            min-width: 100% !important;
+        }
+
+        .hf-col-12 {
             width: 100%;
-            padding: 20px;
+        }
 
-            .topText {
-                color: #000000;
-                font-size: 16px;
-            }
+        .hf-col-3 {
+            flex: 0 0 auto;
+            width: 25%;
 
-            .creditCards {
-                img {
-                    max-height: 60px;
-                }
+            @media screen and (max-width: 568px) {
+                width: 35%;
             }
         }
 
-        #submit-payment {
-            background-color: var(--wp--preset--color--contrast);
-            border-radius: 0.33rem;
-            border-color: var(--wp--preset--color--contrast);
-            border-width: 0;
-            color: var(--wp--preset--color--base);
-            font-family: inherit;
-            font-size: var(--wp--preset--font-size--small);
-            font-style: normal;
-            font-weight: 500;
-            line-height: inherit;
-            padding-top: 0.6rem;
-            padding-right: 1rem;
-            padding-bottom: 0.6rem;
-            padding-left: 1rem;
-            text-decoration: none;
-            margin-top: 15px !important;
-            margin-left: auto !important;
-            margin-right: auto !important;
-            border-radius: 8px;
-            width: 90%;
-        }
+        .hf-col-9 {
+            flex: 0 0 auto;
+            width: 75%;
 
-        #ppLogo {
-            font-family: "Almoni", sans-serif;
-            color: #c5cbcf;
-            text-align: center;
-            font-size: 12px;
-            width: 100%;
-            bottom: 5px;
-            direction: ltr;
-            padding: 1em;
-
-            .hf-image {
-                height: 17px;
-                top: 2px;
-                display: initial;
+            @media screen and (max-width: 568px) {
+                width: 65%;
             }
-        }
 
-        .expiry-wrapper {
-            justify-content: space-between;
-        }
-
-        .container.hostedFields {
-            display: none;
-        }
-
-        #payment-form {
-            display: none;
-            align-items: center;
-            flex-direction: column;
-            flex-wrap: wrap;
-            align-content: flex-start;
-        }
-
-        .__payplus_hosted_fields_err_fld {
-            color: red;
-            border: 1px solid red;
-        }
-
-        input[readonly] {
-            background-color: #eee;
-            outline: 0;
-        }
-
-        .blocks-payplus_loader_hosted {
-            display: none;
-            position: relative;
-            z-index: 999999;
-        }
-
-        .__payplus_hosted_fields_item_fld-wrapper {
-            display: flex;
-            flex-wrap: wrap;
-            align-content: center;
-        }
-
-        .expiries {
-            width: 90%;
-            display: flex;
-            margin: auto;
-            flex-wrap: wrap;
-            justify-content: space-between;
-        }
-
-        input:-internal-autofill-selected {
-            background-color: transparent !important;
-        }
-
-        .col-9 {
             @media screen and (min-width: 467px) {
                 margin-left: 3px;
                 max-width: 74% !important;
             }
         }
+    }
 
-        .expireCvvClass {
-            background-color: white;
-            width: 50%;
-            height: 45px;
-            display: flex;
-            padding: 0 10px 0 10px;
-            justify-content: center;
-            border: 1px solid #E3E6E9 !important;
-            border-top: none !important;
+    .hf-save {
+        margin-top: 10px !important;
+    }
 
-
-            &.right {
-                border-bottom-right-radius: 8px;
-                border-left: none !important;
-            }
-
-            &.left {
-                border-bottom-left-radius: 8px;
-            }
-        }
-
-        .smallCol {
-            flex: 0 0 auto;
-            width: 30%;
-        }
-
-        .justBorder {
-            border: 1px solid #E3E6E9 !important;
-        }
-
-        .pp_iframe_h iframe {
-            border: 0;
-            max-height: 45px;
-            /* min-width: 55px;
-            text-align: center; */
-        }
-
-        .fld-frame {
-            border: none;
-            box-sizing: unset;
-            outline: unset;
-        }
-
-        input {
-            outline: unset;
-        }
-
-        .pp_iframe_h {
-            .form-control {
-                width: 100% !important;
-                padding: 0.5rem 0.75rem !important;
-                font-size: 1rem !important;
-                border: 1px solid #E3E6E9 !important;
-                border-top-left-radius: 8px;
-                border-top-right-radius: 8px;
-                height: 45px;
-                margin: auto !important;
-                text-align: inherit;
-            }
-
-            .forms-control {
-                width: 100% !important;
-                padding: 0.5rem 0.75rem !important;
-                font-size: 1rem !important;
-                border: 1px solid #E3E6E9 !important;
-                border-radius: 8px !important;
-                height: 45px;
-                margin: auto !important;
-                text-align: inherit;
-            }
-        }
-
-        input[type="text" i] {
-            font-size: 20px;
-        }
-
-        .h-fld-wrapper {
-            width: 100%;
-        }
-
-        .fld-wrapper {
-            width: 90%;
-            margin: 0.2em auto 0.2em auto;
-        }
-
-        .hf-row,
-        .hf-main {
-            width: 100%;
-            display: flex;
-
-            @media screen and (max-width: 768px) {
-                min-width: 100% !important;
-            }
-
-            .hf-col-12 {
-                width: 100%;
-            }
-
-            .hf-col-3 {
-                flex: 0 0 auto;
-                width: 25%;
-
-                @media screen and (max-width: 568px) {
-                    width: 35%;
-                }
-            }
-
-            .hf-col-9 {
-                flex: 0 0 auto;
-                width: 75%;
-
-                @media screen and (max-width: 568px) {
-                    width: 65%;
-                }
-
-                @media screen and (min-width: 467px) {
-                    margin-left: 3px;
-                    max-width: 74% !important;
-                }
-            }
-        }
-
-        .hf-save {
-            margin-top: 10px !important;
-        }
-
-        .pp_iframe_h {
-            .hf-main>* {
-                padding: unset !important;
-            }
-        }
-
-        .btn-primary {
-            font-size: 21px !important;
+    .pp_iframe_h {
+        .hf-main>* {
             padding: unset !important;
-            height: 44px !important;
         }
+    }
 
-        .exp {
-            width: 40%;
-        }
+    .btn-primary {
+        font-size: 21px !important;
+        padding: unset !important;
+        height: 44px !important;
+    }
 
-        .seperator {
-            margin-top: 3%;
-            width: auto;
-            color: #E3E6E9;
-        }
+    .exp {
+        width: 40%;
+    }
 
-        .form-select {
-            height: 45px;
-            border: 1px solid #E3E6E9 !important;
-            border-radius: 8px !important;
-            background-color: white;
-            outline: none;
-        }
+    .seperator {
+        margin-top: 3%;
+        width: auto;
+        color: #E3E6E9;
+    }
 
-        #payments-wrapper {
-            display: flex;
-            flex-wrap: wrap;
-            flex-direction: column;
-        }
+    .form-select {
+        height: 45px;
+        border: 1px solid #E3E6E9 !important;
+        border-radius: 8px !important;
+        background-color: white;
+        outline: none;
+    }
 
-        #payments-wrapper label {
-            order: 1;
-            /* Label always appears first */
-        }
+    #payments-wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: column;
+    }
 
-        #payments-wrapper select {
-            order: 2;
-            /* Select field always appears second */
-        }
+    #payments-wrapper label {
+        order: 1;
+        /* Label always appears first */
+    }
 
-        .card-holder-phone-prefix {
-            width: 100%;
-            outline: none;
-            text-align: center;
-        }
+    #payments-wrapper select {
+        order: 2;
+        /* Select field always appears second */
+    }
+
+    .card-holder-phone-prefix {
+        width: 100%;
+        outline: none;
+        text-align: center;
+    }
     </style>
 </head>
 
@@ -488,7 +489,7 @@ $opposite = $locale !== "he_IL" ?  "left" : "right";
                     <div id="ppLogo">
                         <?php echo esc_html__('Powered by ', 'payplus-payment-gateway'); ?>
                         <img class="hf-image"
-                            src="../wp-content/plugins/payplus-payment-gateway/assets/images/payplus-logo-new.png"
+                            src="<?php echo site_url();?>/wp-content/plugins/payplus-payment-gateway/assets/images/payplus-logo-new.png"
                             alt="<?php echo esc_attr__('Pay with Debit or Credit Card', 'payplus-payment-gateway'); ?>" />
                     </div>
                 </div>
