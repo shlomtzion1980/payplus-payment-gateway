1609362a5623a140a96f3e28a8718d0d19e7741f
shlomtzion <shlomtzion@gmai.com>
2024-12-31 18:40:06 +0200

    tweak/set order_awaiting_payment asap

--- includes/wc_payplus_subgateways.php
+++ includes/wc_payplus_subgateways.php
@@ -527,6 +527,7 @@ class WC_PayPlus_Gateway_HostedFields extends WC_PayPlus_Subgateway
         }
         $order = wc_get_order($order_id);
         if ($this->id === "payplus-payment-gateway-hostedfields") {
+            WC()->session->set('order_awaiting_payment', $order_id);
             $hostedClass = new WC_PayPlus_HostedFields($order_id, $order, true);
         }
         return array(
