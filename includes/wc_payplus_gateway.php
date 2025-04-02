93c544fa105bf013815f7715d47053760a610882
ShlomtzPP <shlomtzionPP>
2025-04-02 10:50:37 +0300

    feature/support for multiple POS EMV devices - divided by , and :

--- includes/wc_payplus_gateway.php
+++ includes/wc_payplus_gateway.php
@@ -153,25 +153,7 @@ class WC_PayPlus_Gateway extends WC_Payment_Gateway_CC
         $this->api_key = $this->api_test_mode ? $this->get_option('dev_api_key') ?? null : $this->get_option('api_key');
         $this->secret_key = $this->api_test_mode ? $this->get_option('dev_secret_key') ?? null : $this->get_option('secret_key');
         $this->device_uid = $this->api_test_mode ? $this->get_option('dev_device_uid') ?? null : $this->get_option('device_uid');
-        $cstoreId = 13;
-        if (strlen($this->device_uid) > 0 && isset($cstoreId)) {
-            $currentDevice = $this->device_uid;
-            if (strpos($this->device_uid, ',') !== false) {
-                $devices = explode(',', $this->device_uid);
-
-                foreach ($devices as $device) {
-                    list($storeId, $deviceId) = explode(':', $device);
-                    if ($cstoreId == $storeId) {
-                        $currentDevice = $deviceId;
-                        break;
-                    }
-                }
-            }
-            $this->device_uid = $currentDevice;
-        }
-        // Example usage: Log the storeId and deviceId
-        print_r("Store ID: $cstoreId, Device ID: $this->device_uid");
-        die;
+        $this->device_uid = $this->getPayPlusDeviceId($this->device_uid);
         $this->payment_page_id = $this->api_test_mode ? $this->get_option('dev_payment_page_id') ?? null : $this->get_option('payment_page_id');
         $this->current_time = wp_date('Y-m-d H:i:s', current_time('timestamp'), new DateTimeZone('Asia/Jerusalem'));
         $this->rounding_decimals = ROUNDING_DECIMALS;
@@ -292,7 +274,8 @@ class WC_PayPlus_Gateway extends WC_Payment_Gateway_CC
         // Hook the custom function to the scheduled event
         add_action('payplus_after_process_payment_event', array($this, 'payplus_after_process_payment_function'));
         add_action('woocommerce_checkout_order_processed', [$this, 'pwGiftCardsOnNoPayment'], 10, 3);
-        // add_action('woocommerce_payment_complete', [$this, 'payplusCheckPaymentGatewayId'], 10, 1);
+        add_action('woocommerce_payment_complete', [$this, 'payplusCheckPaymentGatewayId'], 10, 1);
+
         /****** ACTION END ******/
 
         /****** FILTER START ******/
@@ -322,16 +305,40 @@ class WC_PayPlus_Gateway extends WC_Payment_Gateway_CC
         }
     }
 
+    public function getPayPlusDeviceId($deviceUid)
+    {
+        $cStoreId = get_current_user_id();
+        if (strlen($deviceUid) > 0 && isset($cStoreId)) {
+            $currentDevice = $this->device_uid;
+            if (strpos($deviceUid, ',') !== false) {
+                $devices = explode(',', $deviceUid);
+
+                foreach ($devices as $device) {
+                    list($storeId, $deviceId) = explode(':', $device);
+                    if ($cStoreId == $storeId) {
+                        $currentDevice = $deviceId;
+                        break;
+                    }
+                }
+            }
+            $this->device_uid = $currentDevice;
+        }
+        return $this->device_uid;
+    }
+
     public function payplusCheckPaymentGatewayId($order_id)
     {
         $order = wc_get_order($order_id);
 
         // Get the payment gateway ID
         $payment_gateway_id = $order->get_payment_method();
+        $WC_PayPlus_Admin_Payments = new WC_PayPlus_Admin_Payments;
 
         // Check if the payment gateway ID matches your target ID
         if ($payment_gateway_id === 'pos_card') {
             // Perform your custom logic here
+            $_wpnonce = wp_create_nonce('ajax_payplus_generate_link_payment');
+            $WC_PayPlus_Admin_Payments->ajax_payplus_generate_link_payment($order_id, $_wpnonce);
             error_log('Order #' . $order_id . ' was paid using the POS Card gateway.');
         }
     }
