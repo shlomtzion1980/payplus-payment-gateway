ef4507250a1a75dd27e3abb4cfabc4de64af877b
ShlomtzPP <shlomtzionPP>
2025-06-10 17:09:34 +0300

    tweak/paying vat no matter what!

--- includes/wc_payplus_invoice.php
+++ includes/wc_payplus_invoice.php
@@ -909,7 +909,7 @@ class PayplusInvoice
         $WC_PayPlus_Gateway = $this->get_main_payplus_gateway();
         $payingVatAllOrder = $WC_PayPlus_Gateway->settings['paying_vat_all_order'] === "yes";
         $changevatInEilat = $WC_PayPlus_Gateway->change_vat_in_eilat && $WC_PayPlus_Gateway->payplus_check_is_vat_eilat($order_id);
-        $OtherVatCountry = $this->payplus_check_vat_payment($order_id);
+        $OtherVatCountry = $this->payplus_check_vat_payment($order_id) || $WC_PayPlus_Gateway->paying_vat == "1";
         foreach ($productsItems as $key => $productsItem) {
 
             if ($payingVatAllOrder) {
